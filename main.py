409b32a6e51a26fbb8bcd07c42b11ea3261f8e6c
Priyanshu Sarangi <sarangipriyanshu@gmail.com>
2023-05-29 20:40:14 +0530

    Response Generator

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+import streamlit as st
+from langchain.llms import OpenAI
+from langchain.agents import create_csv_agent
+from dotenv import load_dotenv
+import os
+# creating GUI 
 def main():
-    print("Hello World")
+
+    # to make sure env variables are read 
+    load_dotenv()
+
+     
+
+    st.set_page_config(page_title="CSV CHATGPT")
+    st.header("CHATGPT FOR CSV")
+
+    user_csv=st.file_uploader(label="Upload The CSV file",type="CSV")
+
+# action when user has entered his csv
+    if user_csv is not None:
+       question=st.text_input("Ask Question About YOur CSV FILE")
+
+       language_model=OpenAI(temperature=0)
+       agent=create_csv_agent(language_model,user_csv)
+
+
+    #    now agent is used to answer the question 
+       if question is not None and question!="":
+              
+          response=agent.run(question)
+          st.write(response)
+   
 
 if __name__=="__main__":
     main()    

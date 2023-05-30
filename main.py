5d02062bcda38a24c14d3968a776a1f92916a487
Priyanshu Sarangi <sarangipriyanshu@gmail.com>
2023-05-30 18:42:54 +0530

    Error Issue

--- main.py
+++ main.py
@@ -9,26 +9,28 @@ def main():
     # to make sure env variables are read 
     load_dotenv()
 
-     
-
     st.set_page_config(page_title="CSV CHATGPT")
     st.header("CHATGPT FOR CSV")
 
-    user_csv=st.file_uploader("Upload The CSV file",type="csv")
+    user_csv=st.file_uploader("Upload The CSV file",type=['CSV'])
 
 # action when user has entered his csv
     if user_csv is not None:
-       question=st.text_input("Ask Question About YOur CSV FILE")
+          question=st.text_input("Ask Question About YOur CSV FILE")
+       
+         
+        
+          llm=OpenAI(temperature=0.7)
+          agent=create_csv_agent(llm,user_csv,verbose=True)
 
-       language_model=OpenAI(temperature=0)
-       agent=create_csv_agent(language_model,user_csv)
+       
 
 
     #    now agent is used to answer the question 
-       if question is not None and question!="":
+          if question is not None and question!="":
               
-          response=agent.run(question)
-          st.write(response)
+            response=agent.run(question)
+            st.write(response)
    
 
 if __name__=="__main__":
